24ce713bcfccd6c13de7a419178500de88347656
hodynguyen <nguyenthanhdat23012003@gmail.com>
2025-11-24 02:47:43 +0700

    Create Public Route Table

---
+++ infra/vpc/route_tables.tf
@@ -0,0 +1,26 @@
+# -----------------------------------------
+# PUBLIC ROUTE TABLE
+# -----------------------------------------
+
+# 1. Create Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "acme-public-rt"
+  }
+}
+
+# 2. Add route: Internet traffic → IGW
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# 3. Associate public subnets to public route table
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public[*].id)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
