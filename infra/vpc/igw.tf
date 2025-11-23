c1c3e627d57744e42b9e92ee4b8a51062d50dbe6
hodynguyen <nguyenthanhdat23012003@gmail.com>
2025-11-24 02:22:29 +0700

    Create Internet Gateway

---
+++ infra/vpc/igw.tf
@@ -0,0 +1,10 @@
+# ---------------------------
+# INTERNET GATEWAY
+# ---------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "acme-igw"
+  }
+}
